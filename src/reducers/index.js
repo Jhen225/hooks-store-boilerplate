7d77c1b8ddbcdd966aba182b59db4caad3b60b59
jhen225 <jhen103093@gmail.com>
2019-03-17 18:45:44 -0500

    added some of the example functionality back and changed README

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,14 @@
 import { INCREMENT, DECREMENT } from '../actions/types';
 export const reducer = (state, action) => {
     switch (action.type) {
-        
+        case INCREMENT:
+            console.log("increment",state)
+            return { ...state, count: state.count + 1 }
+        case DECREMENT:
+            console.log("decrement",state)
+            return { ...state, count: state.count - 1 }
+        default:
+            console.log(state)
+            return state;
     }
 }
